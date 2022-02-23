faf6a0791efbdc994d4480adf0b4ed216004e5c4
yj971020 <yj971020@naver.com>
2022-02-23 15:15:38 +0900

    22일 스프링 부트 챌린지

--- CodingTest Study/src/skTest/Solution4.java
+++ CodingTest Study/src/skTest/Solution4.java
@@ -8,17 +8,28 @@ public class Solution4 {
 	 public static int[] solution(String[] rsp3) {
 	        int size = rsp3.length; //몇 판
 	        int[] win=new int[3]; // 누적점수
-	        
+	        int j=0;
 	        for(String s :rsp3) {
-	        	s.replace("S", "1");
-	        	s.replace("R","2");
-	        	s.replace("p","3");
-	        	String[] temp = s.split("");
-	        	System.out.println(Arrays.toString(temp));
+	        	String[] temp = s.split(""); // 나눔 
+	        	int[] rsp=new int[3]; // 숫자로 저장 배열	        	       
 	        	if(temp[0].equals(temp[1]) & temp[1].equals(temp[2])) {
 	        		continue;
 	        	}else {
-	        		
+	        		for(int i=0;i<3;i++) {
+	        		switch (temp[i]) {
+					case "S":
+						rsp[i]=1;
+						break;
+					case "R":
+						rsp[i]=2;
+						break;
+					case "P":
+						rsp[i]=3;
+						break;
+					default:
+						break;
+					}
+	        			}
 	        	}
 	        }
 	        
